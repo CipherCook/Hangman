458a9339b0a24c1a2464f47f66631cbf38a7533e
Utkarsh Sharma <utkarsh.mask@gmail.com>
2020-06-23 23:26:23 +0530

    Add files via upload

---
+++ Script.py
@@ -0,0 +1,47 @@
+import tkinter as tk
+import random
+deathstate = 1
+def display1():
+    #tkinter code for starting animation
+    while gameon==True:
+        if deathstate==1:
+            #tk for hangman state 1
+            print()
+            #proceed similarly for 6 states of death of hangman
+        
+    if gameon==False:
+        print()
+        #ending display
+        #restart?
+import time
+
+#welcoming the user
+name1 = input("Player 1, What is your name?")
+
+print("hello " + name1)
+
+name2= input("Player 2, what is your name?")
+print("hello  ",name2)
+time.sleep(1)
+print ("Start guessing...")
+#call display 1 bumblebee
+gameon = True
+
+def get_secret_word():
+
+    random_wordbank = ["lemon","orange","grapefruit","apple","lychee","pear","banana","melon","watermelon","honey"] #word bank of potential secret words
+    pick_word = random.randint(0, len(random_wordbank)-1) #choose a random integer in range of wordbank length to use as the secret word
+    return random_wordbank[pick_word]
+
+secret= get_secret_word()
+turns = 6
+fails=0
+while turns>0:
+    char=input("please enter a character")
+    for i in word:
+        if i==char:
+            print(char)
+    
+
+        
+        
